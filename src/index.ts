3b7faa0b0e4ba82e8419bd442126ac6f03578041
Chris Hills <chris.hills@communitybrands.com>
2020-04-21 15:55:34 -0500

    export IInputProps

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@
 export { Input } from "./input/Input";
 export { Fieldsets } from "./fieldset/Fieldsets";
 export { Form } from "./form/Form";
+export { IInputProps } from "./input/IInputProps";
 export { IFormMeta, IInputTemplateProps, IArrayItemTemplateProps, IFieldsetTemplate } from "./form/IFormMeta";
